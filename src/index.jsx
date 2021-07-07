5f76ac3f2535e16dada3203ebf35a4633f85d7f7
Jos3s <karatedkgt17@gmail.com>
2021-07-07 09:38:54 -0300

    Importando tema light

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from '../src/styles/global';
-import { theme } from '../src/styles/theme';
+import { themeLight } from '../src/styles/theme';
 import { Routers } from './pages/router';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={themeLight}>
       <Routers />
       <GlobalStyles />
     </ThemeProvider>
