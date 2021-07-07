3382b3994dee0a0836b9757843ea4550b85ba4a5
Jos3s <karatedkgt17@gmail.com>
2021-07-07 10:12:06 -0300

    Adicionano botão de troca de tema

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,23 @@
 import P from 'prop-types';
 import * as Styled from './styles';
 import { Link as RRDLink } from 'react-router-dom';
+import { BsSun, BsMoon } from 'react-icons/bs';
 
+import { useTheme } from '../../hooks/useTheme';
 import { Heading } from '../Heading';
+import { Button } from '../Button';
 import { Link } from '../Link';
 
 export const Header = () => {
+  const { toggleTheme, themeTitle } = useTheme();
+
   return (
     <Styled.Container>
       <Heading>
         <RRDLink to="/">Books finder</RRDLink>
+        <Button onClick={toggleTheme} border="none" title="Switch Theme" p="small">
+          {themeTitle === 'LightTheme' ? <BsSun size="1.5em" /> : <BsMoon size="1.5em" />}
+        </Button>
       </Heading>
       <Heading as="h2" size="xsmall">
         by{' '}
