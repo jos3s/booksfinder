ab41f90f4df7281b79fadf7134d3f8efc453d05c
Jos3s <karatedkgt17@gmail.com>
2021-07-09 18:51:01 -0300

    Usando styled-system no Search

--- src/components/Search/styles.js
+++ src/components/Search/styles.js
@@ -1,18 +1,12 @@
 import styled from 'styled-components';
-import { color, space, grid, typography, border } from 'styled-system';
+import { color, space, grid, typography, border, compose, flexbox } from 'styled-system';
 import { Button } from '../Button/styles';
 
 export const Form = styled.form`
   display: flex;
-  flex-direction: column;
-  justify-content: space-between;
   max-width: 600px;
 
-  ${grid}
-
-  @media (min-width: 500px) {
-    flex-direction: row;
-  }
+  ${compose(grid, flexbox)}
 
   ${Button} {
     flex: 1;
@@ -20,21 +14,16 @@ export const Form = styled.form`
 `;
 
 export const Input = styled.input`
-  ${color}
-  ${space}
-  ${typography}
-  ${border}
+  ${compose(color, space, typography)}
 
   outline: none;
-  width: 100%;
+  max-width: 380px;
   flex: 3;
 
   border: 1px solid ${({ theme }) => theme.colors.gray[400]};
+  border-radius: ${({ theme }) => theme.radii.sm};
   background: ${({ theme }) => theme.colors.gray[50]};
-
-  @media (min-width: 500px) {
-    width: 50rem;
-  }
+  padding: ${({ theme }) => theme.space.xsmall};
 
   &:focus {
     border-color: ${({ theme }) => theme.colors.gray[600]};
