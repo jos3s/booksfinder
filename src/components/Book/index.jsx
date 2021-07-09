2810c3749cd4169d91a8737bb7a5437e32308afd
Jos3s <karatedkgt17@gmail.com>
2021-07-09 18:53:15 -0300

    Utilizando compose do styled-system

--- src/components/Book/index.jsx
+++ src/components/Book/index.jsx
@@ -5,38 +5,49 @@ import { Heading } from './../Heading';
 import { Link } from 'react-router-dom';
 
 import { Button } from '../../components/Button';
+import { Text } from '../../components/Text';
+import { Image } from '../../components/Image';
 import { Card } from '../Card';
 
 export const Book = ({ srcImg, title, authors, published, id }) => {
   return (
-    <Styled.Book>
+    <Styled.Wrapper>
       <Card>
-        <Styled.Img src={srcImg} borderRadius="sm" />
-        <Styled.Content>
-          <Styled.Info>
+        <Image
+          src={srcImg}
+          alt={title}
+          borderRadius="sm"
+          width="12.8rem"
+          height="20rem"
+        />
+        <Styled.Content justifyContent={['flex-start', 'space-evenly']}>
+          <Styled.Wrapper>
             <Heading size="small" as="h2">
-              {title}
+              <Link to={`books/${id}`}>{title}</Link>
             </Heading>
-            <Heading size="xsmall" as="h3">
+
+            <Heading size="xsmall" as="h3" my="xsmall">
               Author: {authors?.join(', ') || 'Unknown'}
             </Heading>
-            <p>
+
+            <Text my="small" fontSize="small">
               Published: <time>{published || 'Unknown'}</time>
-            </p>
-          </Styled.Info>
+            </Text>
+          </Styled.Wrapper>
+
           <Link to={`books/${id}`}>
             <Button>More info</Button>
           </Link>
         </Styled.Content>
       </Card>
-    </Styled.Book>
+    </Styled.Wrapper>
   );
 };
 
 Book.propTypes = {
   srcImg: P.string,
-  title: P.string,
+  title: P.string.isRequired,
   authors: P.arrayOf(P.string),
-  published: P.string,
-  id: P.string,
+  published: P.string.isRequired,
+  id: P.string.isRequired,
 };
